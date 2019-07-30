f40280f11c4bae54649363f68778798ee57b691b
frankdhondt <f.d.hondt@telenet.be>
2019-07-30 19:23:46 +0200

    includePaths added to remove problems in vscode

--- Firmware/WeavingLoom.ino
+++ Firmware/WeavingLoom.ino
@@ -1,6 +1,9 @@
 //
 // ATTENTION : Inspect code to understand what is does before uploading to loom
 //
+#include "pins_arduino.h"
+#include "wiring_private.h"
+
 // RAMPS X inslag 1
   const int dirPinInslag1 = A1;
   const int stepPinInslag1 = A0;
